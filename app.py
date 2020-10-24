2b1de281fe970fca61fba58c5d9e6f6bf063ffb7
Muhammad Shahid <info@users.noreply.github.com>
2020-10-24 16:33:58 +0500

    Added HTML file

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return """<h1>Hello World</h1>
-    
-    <h1>Muhammad Shahid</h>
-    
-    """
-
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
